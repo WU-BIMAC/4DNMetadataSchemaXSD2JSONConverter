360e22f3fc545dd118123148808424f46384ec80
akramhar <alex.83@bluewin.ch>
2020-12-10 13:29:27 -0500

    fixed new tentative dimensions

--- src/edu/umassmed/TXT2JSONDimensionConverter/TXT2JSONDimensionConverter.java
+++ src/edu/umassmed/TXT2JSONDimensionConverter/TXT2JSONDimensionConverter.java
@@ -13,12 +13,12 @@ import java.util.Map;
 public class TXT2JSONDimensionConverter {
 	final Map<String, Map<String, List<String[]>>> positions;
 	final Map<String, Map<String, List<String[]>>> dimensions;
-
+	
 	public TXT2JSONDimensionConverter() {
 		this.positions = new LinkedHashMap<String, Map<String, List<String[]>>>();
 		this.dimensions = new LinkedHashMap<String, Map<String, List<String[]>>>();
 	}
-
+	
 	public void writeDimensionsFile() throws IOException {
 		final FileWriter fw = new FileWriter(
 				"./dimensionsV2/MicroscopeDimensions.json");
@@ -34,7 +34,7 @@ public class TXT2JSONDimensionConverter {
 			final String tabs = "\t";
 			bw.write(tabs + "\"" + type + "\":{\n");
 			for (final String key : typePositions.keySet()) {
-				
+
 				final List<String[]> coords = typePositions.get(key);
 				final List<String[]> dims = typeDimensions.get(key);
 				final String tabs2 = tabs + "\t";
@@ -46,19 +46,19 @@ public class TXT2JSONDimensionConverter {
 				} else {
 					bw.write("{\n");
 				}
-				
+
 				for (int i = 0; i < coords.size(); i++) {
 					final String[] singleCoord = coords.get(i);
 					final String[] singleDims = dims.get(i);
 					if (coords.size() > 1) {
 						bw.write(tabs3 + "{\n");
 					}
-					
+
 					bw.write(tabs3 + "\t\"x\":" + singleCoord[0] + ",\n");
 					bw.write(tabs3 + "\t\"y\":" + singleCoord[1] + ",\n");
 					bw.write(tabs3 + "\t\"w\":" + singleDims[0] + ",\n");
 					bw.write(tabs3 + "\t\"h\":" + singleDims[1] + "\n");
-					
+
 					if (coords.size() > 1) {
 						if (i < (coords.size() - 1)) {
 							bw.write(tabs3 + "},\n");
@@ -67,7 +67,7 @@ public class TXT2JSONDimensionConverter {
 						}
 					}
 				}
-				
+
 				if (coords.size() > 1) {
 					bw.write(tabs2 + "]");
 				} else {
@@ -92,12 +92,12 @@ public class TXT2JSONDimensionConverter {
 		bw.close();
 		fw.close();
 	}
-	
+
 	public void parseDimensionsFile(final String type) throws IOException {
 		final String f = "./dimensionsV2/" + type + ".txt";
 		final FileReader fr = new FileReader(f);
 		final BufferedReader br = new BufferedReader(fr);
-
+		
 		Map<String, List<String[]>> typePositions;
 		Map<String, List<String[]>> typeDimensions;
 		if (this.positions.containsKey(type)) {
@@ -113,8 +113,12 @@ public class TXT2JSONDimensionConverter {
 		
 		String line = br.readLine();
 		while (line != null) {
+			if (line.startsWith("//") || line.equals("")) {
+				line = br.readLine();
+				continue;
+			}
 			final String[] values = line.split(":");
-
+			
 			final String[] coord = values[1].split(",");
 			final String[] dims = values[2].split(",");
 			List<String[]> list;
@@ -139,7 +143,7 @@ public class TXT2JSONDimensionConverter {
 		this.positions.put(type, typePositions);
 		this.dimensions.put(type, typeDimensions);
 	}
-	
+
 	public static void main(final String[] args) {
 		final TXT2JSONDimensionConverter conv = new TXT2JSONDimensionConverter();
 		try {
