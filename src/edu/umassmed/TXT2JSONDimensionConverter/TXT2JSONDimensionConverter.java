26e17abe3a84d270680220a8754e05027c2bb0bc
akramhar <alex.83@bluewin.ch>
2021-03-12 22:30:51 -0500

    added upright and rotate to dimensions V2 + fixes

--- src/edu/umassmed/TXT2JSONDimensionConverter/TXT2JSONDimensionConverter.java
+++ src/edu/umassmed/TXT2JSONDimensionConverter/TXT2JSONDimensionConverter.java
@@ -13,12 +13,14 @@ import java.util.Map;
 public class TXT2JSONDimensionConverter {
 	final Map<String, Map<String, Map<String, List<String[]>>>> positions;
 	final Map<String, Map<String, Map<String, List<String[]>>>> dimensions;
-	
+	final Map<String, Map<String, Map<String, List<String[]>>>> rotates;
+
 	public TXT2JSONDimensionConverter() {
 		this.positions = new LinkedHashMap<String, Map<String, Map<String, List<String[]>>>>();
 		this.dimensions = new LinkedHashMap<String, Map<String, Map<String, List<String[]>>>>();
+		this.rotates = new LinkedHashMap<String, Map<String, Map<String, List<String[]>>>>();
 	}
-	
+
 	public void writeDimensionsFile() throws IOException {
 		final FileWriter fw = new FileWriter(
 				"./dimensionsV2/MicroscopeDimensions.json");
@@ -32,6 +34,8 @@ public class TXT2JSONDimensionConverter {
 					.get(type);
 			final Map<String, Map<String, List<String[]>>> typeDimensions = this.dimensions
 					.get(type);
+			final Map<String, Map<String, List<String[]>>> typeRotates = this.rotates
+					.get(type);
 			final String tabs = "\t";
 			bw.write(tabs + "\"" + type + "\":{\n");
 			counter2 = 0;
@@ -42,14 +46,17 @@ public class TXT2JSONDimensionConverter {
 						.get(key);
 				final Map<String, List<String[]>> groupDims = typeDimensions
 						.get(key);
+				final Map<String, List<String[]>> groupRotates = typeRotates
+						.get(key);
 				counter3 = 0;
 				for (final String groupKey : groupCoord.keySet()) {
 					final String tabs3 = tabs2 + "\t";
 					bw.write(tabs3 + "\"" + groupKey + "\":");
-					
+
 					final List<String[]> coords = groupCoord.get(groupKey);
 					final List<String[]> dims = groupDims.get(groupKey);
-
+					final List<String[]> rotates = groupRotates.get(groupKey);
+					
 					String tabs4 = tabs3;
 					if (coords.size() > 1) {
 						bw.write("[\n");
@@ -57,19 +64,25 @@ public class TXT2JSONDimensionConverter {
 					} else {
 						bw.write("{\n");
 					}
-					
+
 					for (int i = 0; i < coords.size(); i++) {
 						final String[] singleCoord = coords.get(i);
 						final String[] singleDims = dims.get(i);
+						final String[] singleRotate = rotates.get(i);
 						if (coords.size() > 1) {
 							bw.write(tabs4 + "{\n");
 						}
-						
+
 						bw.write(tabs4 + "\t\"x\":" + singleCoord[0] + ",\n");
 						bw.write(tabs4 + "\t\"y\":" + singleCoord[1] + ",\n");
 						bw.write(tabs4 + "\t\"w\":" + singleDims[0] + ",\n");
 						bw.write(tabs4 + "\t\"h\":" + singleDims[1] + "\n");
-						
+						if ((singleRotate.length > 0)
+								&& (singleRotate[0] != null)) {
+							bw.write(tabs4 + "\t\"r\":" + singleRotate[0]
+									+ "\n");
+						}
+
 						if (coords.size() > 1) {
 							if (i < (coords.size() - 1)) {
 								bw.write(tabs4 + "},\n");
@@ -78,7 +91,7 @@ public class TXT2JSONDimensionConverter {
 							}
 						}
 					}
-					
+
 					if (coords.size() > 1) {
 						bw.write(tabs3 + "]");
 					} else {
@@ -111,14 +124,15 @@ public class TXT2JSONDimensionConverter {
 		bw.close();
 		fw.close();
 	}
-
+	
 	public void parseDimensionsFile(final String type) throws IOException {
 		final String f = "./dimensionsV2/" + type + ".txt";
 		final FileReader fr = new FileReader(f);
 		final BufferedReader br = new BufferedReader(fr);
-		
+
 		Map<String, Map<String, List<String[]>>> typePositions;
 		Map<String, Map<String, List<String[]>>> typeDimensions;
+		Map<String, Map<String, List<String[]>>> typeRotates;
 		if (this.positions.containsKey(type)) {
 			typePositions = this.positions.get(type);
 		} else {
@@ -129,23 +143,32 @@ public class TXT2JSONDimensionConverter {
 		} else {
 			typeDimensions = new LinkedHashMap<String, Map<String, List<String[]>>>();
 		}
-		
+		if (this.rotates.containsKey(type)) {
+			typeRotates = this.rotates.get(type);
+		} else {
+			typeRotates = new LinkedHashMap<String, Map<String, List<String[]>>>();
+		}
+
 		String line = br.readLine();
 		while (line != null) {
 			if (line.startsWith("//") || line.equals("")) {
 				line = br.readLine();
 				continue;
 			}
-			
+
 			final String[] values = line.split(":");
-			
+
 			final String[] coord = values[1].split(",");
 			final String[] dims = values[2].split(",");
+			final String[] rotate = new String[1];
+			if (values.length > 3) {
+				rotate[0] = values[3];
+			}
 			Map<String, List<String[]>> map;
 			List<String[]> list;
-			
-			final String key = values[0];
 
+			final String key = values[0];
+			
 			String currentKey = values[0];
 			String currentSubKey = "General";
 			if (key.contains("#")) {
@@ -167,7 +190,7 @@ public class TXT2JSONDimensionConverter {
 			list.add(coord);
 			map.put(currentSubKey, list);
 			typePositions.put(currentKey, map);
-
+			
 			if (typeDimensions.keySet().contains(currentKey)) {
 				map = typeDimensions.get(currentKey);
 				if (map.keySet().contains(currentSubKey)) {
@@ -182,18 +205,36 @@ public class TXT2JSONDimensionConverter {
 			list.add(dims);
 			map.put(currentSubKey, list);
 			typeDimensions.put(currentKey, map);
+			
+			if (typeRotates.keySet().contains(currentKey)) {
+				map = typeRotates.get(currentKey);
+				if (map.keySet().contains(currentSubKey)) {
+					list = map.get(currentSubKey);
+				} else {
+					list = new ArrayList<String[]>();
+				}
+			} else {
+				map = new LinkedHashMap<String, List<String[]>>();
+				list = new ArrayList<String[]>();
+			}
+			list.add(rotate);
+			map.put(currentSubKey, list);
+			typeRotates.put(currentKey, map);
+
 			line = br.readLine();
 		}
 		br.close();
 		fr.close();
 		this.positions.put(type, typePositions);
 		this.dimensions.put(type, typeDimensions);
+		this.rotates.put(type, typeRotates);
 	}
-
+	
 	public static void main(final String[] args) {
 		final TXT2JSONDimensionConverter conv = new TXT2JSONDimensionConverter();
 		try {
 			conv.parseDimensionsFile("InvertedMicroscopeStand");
+			conv.parseDimensionsFile("UprightMicroscopeStand");
 			conv.writeDimensionsFile();
 		} catch (final IOException ex) {
 			ex.printStackTrace();
